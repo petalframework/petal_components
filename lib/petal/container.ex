bf87d05be98d1ddb41111def6cbc23a4a0e6e069
Matthew Platts <plattsmw@gmail.com>
2021-10-26 11:21:30 +1100

    Fix container

--- lib/petal/container.ex
+++ lib/petal/container.ex
@@ -1,10 +1,16 @@
 defmodule Petal.Container do
   use Phoenix.Component
 
-  def render(assigns) do
+  def full_width(assigns) do
     ~H"""
-    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div cslass="max-w-3xl mx-auto">
+    <div class={"
+      mx-auto max-w-7xl sm:px-6 lg:px-8
+
+      #{if assigns[:padding_on_mobile], do: "px-4", else: ""}
+      #{if assigns[:class], do: @class, else: ""}
+    "}>
+      <div class="max-w-3xl mx-auto">
+        <%= render_slot(@inner_block) %>
       </div>
     </div>
     """
