f0ef18244ff769bb57ced96932cd8b2d4ed159ad
nhobes <nhobes@gmail.com>
2021-10-29 13:29:44 +1100

    button test

--- test/petal/button_test.exs
+++ test/petal/button_test.exs
@@ -3,11 +3,33 @@ defmodule Petal.ButtonTest do
 
   test "button" do
     html =
-      Petal.Button.render(%{type: "a", label: "Press me", href: "/"})
+      Petal.Button.render(%{
+        __changed__: "",
+        type: "a",
+        label: "Press me",
+        href: "/"
+      })
       |> heex_to_string()
 
     assert html =~ "Press me"
     assert html =~ "<a"
     assert html =~ "href"
+  end
+
+  test "button with phx-click" do
+    html =
+      Petal.Button.render(%{
+        __changed__: "",
+        type: "a",
+        label: "Press me",
+        href: "/",
+        "phx-click": "click_event"
+      })
+      |> heex_to_string()
+
+    assert html =~ "Press me"
+    assert html =~ "<a"
+    assert html =~ "href"
+    assert html =~ "phx-click"
   end
 end
