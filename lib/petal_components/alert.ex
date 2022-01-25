cb15425912284a801603bb16e2cc45ebe57c5d03
Nicolas Hoban <nhobes@gmail.com>
2022-01-26 06:28:28 +1100

    Refactor alert and change colors

--- lib/petal_components/alert.ex
+++ lib/petal_components/alert.ex
@@ -11,26 +11,44 @@ defmodule PetalComponents.Alert do
       |> assign_new(:label, fn -> nil end)
       |> assign_new(:state, fn -> "info" end)
       |> assign_new(:heading, fn -> nil end)
+      |> assign_new(:with_icon, fn -> nil end)
       |> assign_new(:inner_block, fn -> nil end)
       |> assign_new(:classes, fn -> alert_classes(assigns) end)
+      |> assign_new(:close_button_properties, fn -> nil end)
 
     ~H"""
     <%= unless label_blank?(@label, @inner_block) do %>
       <div class={@classes}>
-        <div class="flex-shrink-0">
-          <Heroicons.Outline.render icon={get_icon(@state)} />
-        </div>
+        <%= if @with_icon do %>
+          <div class="self-start flex-shrink-0 pt-1">
+            <Heroicons.Solid.render icon={get_icon(@state)} />
+          </div>
+        <% end %>
 
-        <div class="flex-0">
-          <%= if @heading do %>
-            <div class="mb-2 font-bold"><%= @heading %></div>
-          <% end %>
+        <div class="w-full flex-0">
+          <div class="flex items-start justify-between">
+            <div>
+              <%= if @heading do %>
+                <div class="pt-1 font-bold leading-6">
+                  <%= @heading %>
+                </div>
+              <% end %>
 
-          <%= if @inner_block do %>
-            <%= render_slot(@inner_block) %>
-          <% else %>
-            <%= @label %>
-          <% end %>
+              <div class="py-1 font-medium leading-6">
+                <%= if @inner_block do %>
+                  <%= render_slot(@inner_block) %>
+                <% else %>
+                  <%= @label %>
+                <% end %>
+              </div>
+            </div>
+
+            <%= if @close_button_properties do %>
+              <button class={Enum.join(["p-2 mouse-hover flex hover:rounded ", " ", get_dismiss_icon_classes(@state), " "])} {@close_button_properties}>
+                <Heroicons.Solid.x class="self-start w-4 h-4" />
+              </button>
+            <% end %>
+          </div>
         </div>
       </div>
     <% end %>
@@ -43,7 +61,7 @@ defmodule PetalComponents.Alert do
       class: opts[:class] || "",
     }
 
-    base_classes = "w-full flex text-sm rounded-xl justify-start focus:outline-none font-medium leading-6 px-4 py-2 gap-3"
+    base_classes = "w-full flex text-sm rounded items-center focus:outline-none px-4 py-2 gap-3"
     state_css = get_state_classes(opts.state)
     custom_classes = opts.class
 
@@ -55,10 +73,15 @@ defmodule PetalComponents.Alert do
     |> Enum.join(" ")
   end
 
-  defp get_state_classes("info"), do: "text-blue-600 bg-blue-100 dark:bg-[#071418] dark:text-blue-200"
-  defp get_state_classes("success"), do: "text-green-600 bg-green-100 dark:bg-[#0C130D] dark:text-green-200"
-  defp get_state_classes("warning"), do: "text-yellow-600 bg-yellow-100 dark:bg-[#191207] dark:text-yellow-200"
-  defp get_state_classes("danger"), do: "text-red-600 bg-red-100 dark:bg-[#160B0B] dark:text-red-200"
+  defp get_state_classes("info"), do: "text-blue-800 bg-blue-100 dark:bg-blue-200 dark:text-blue-800"
+  defp get_state_classes("success"), do: "text-green-800 bg-green-100 dark:bg-green-200 dark:text-green-800"
+  defp get_state_classes("warning"), do: "text-yellow-800 bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-800"
+  defp get_state_classes("danger"), do: "text-red-800 bg-red-100 dark:bg-red-200 dark:text-red-800"
+
+  defp get_dismiss_icon_classes("info"), do: "bg-blue-100 dark:bg-blue-200 hover:bg-blue-200 dark:hover:bg-blue-300 hover:text-blue-800 dark:hover:text-blue-900"
+  defp get_dismiss_icon_classes("success"), do: "bg-green-100 dark:bg-green-200 hover:bg-green-200 dark:hover:bg-green-300 hover:text-green-800 dark:hover:text-green-900"
+  defp get_dismiss_icon_classes("warning"), do: "bg-yellow-100 dark:bg-yellow-200 hover:bg-yellow-200 dark:hover:bg-yellow-300 hover:text-yellow-800 dark:hover:text-yellow-900"
+  defp get_dismiss_icon_classes("danger"), do: "bg-red-100 dark:bg-red-200 hover:bg-red-200 dark:hover:bg-red-300 hover:text-red-800 dark:hover:text-red-900"
 
   defp get_icon("info"), do: :information_circle
   defp get_icon("success"), do: :check_circle
