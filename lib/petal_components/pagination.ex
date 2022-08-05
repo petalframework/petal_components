fb551210f243b72fa9aadfa923ff04667141f212
Remco Schoeman <remco.schoeman@logiqs.nl>
2022-08-05 22:52:20 +0200

    Fixed pagination control if users set sibling_count to less than 1 or boundary_count less than 1.
    if boundary count is set to zero, no ellipsis or boundary pages are shown.
    if sibling count is set to zero only the current page is shown.
    if total_pages is 1 or less only the button for page one is shown.

--- lib/petal_components/pagination.ex
+++ lib/petal_components/pagination.ex
@@ -33,7 +33,7 @@ defmodule PetalComponents.Pagination do
     <div {@rest} class={"#{@class} flex"}>
       <ul class="inline-flex -space-x-px text-sm font-medium">
         <%= for item <- get_items(@total_pages, @current_page, @sibling_count, @boundary_count) do %>
-          <%= if item.type == "previous" do %>
+          <%= if item.type == "previous" and item.enabled? do %>
             <div>
               <Link.link link_type={@link_type} to={get_path(@path, item.number, @current_page)} class="mr-2 inline-flex items-center justify-center rounded leading-5 px-2.5 py-2 bg-white hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 border dark:border-gray-700 border-gray-200 text-gray-600 hover:text-gray-800">
                 <Heroicons.Solid.chevron_left class="w-5 h-5 text-gray-600 dark:text-gray-400" />
@@ -43,8 +43,8 @@ defmodule PetalComponents.Pagination do
 
           <%= if item.type == "page" do %>
             <li>
-              <%= if item.number == @current_page do %>
-                <span class={get_box_class(item, true)}><%= item.number %></span>
+              <%= if item.current? do %>
+                <span class={get_box_class(item)}><%= item.number %></span>
               <% else %>
                 <Link.link link_type={@link_type} to={get_path(@path, item.number, @current_page)} class={get_box_class(item)}>
                   <%= item.number %>
@@ -59,7 +59,7 @@ defmodule PetalComponents.Pagination do
             </li>
           <% end %>
 
-          <%= if item.type == "next" do %>
+          <%= if item.type == "next" and item.enabled? do %>
             <div>
               <Link.link link_type={@link_type} to={get_path(@path, item.number, @current_page)} class="ml-2 inline-flex items-center justify-center rounded leading-5 px-2.5 py-2 bg-white hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:border-gray-700 border border-gray-200 text-gray-600 hover:text-gray-800">
                 <Heroicons.Solid.chevron_right class="w-5 h-5 text-gray-600 dark:text-gray-400" />
@@ -73,108 +73,132 @@ defmodule PetalComponents.Pagination do
   end
 
   defp get_items(total_pages, current_page, sibling_count, boundary_count) do
-    start_pages = 1..min(boundary_count, total_pages) |> Enum.to_list()
-    end_pages_start = max(total_pages - boundary_count + 1, boundary_count + 1)
-    end_pages_end = total_pages
-    end_pages = end_pages_start..end_pages_end |> Enum.to_list()
+    total_pages = max(1, total_pages)
+    current_page = max(1, min(current_page, total_pages))
+    boundary_count = max(0, min(boundary_count, total_pages))
+    sibling_count = max(0, sibling_count)
 
-    siblings_start =
-      max(
-        min(current_page - sibling_count, total_pages - boundary_count - sibling_count * 2 - 1),
-        boundary_count + 2
-      )
+    siblings_size = 1 + 2 * sibling_count
+    start_siblings = max(1, min(current_page - sibling_count, total_pages - siblings_size - boundary_count + 1))
+    end_siblings = min(max(current_page + sibling_count, siblings_size + boundary_count), total_pages)
 
-    siblings_end =
-      min(
-        max(current_page + sibling_count, boundary_count + sibling_count * 2 + 2),
-        if(length(end_pages) > 0, do: List.first(end_pages) - 2, else: total_pages - 1)
-      )
-
-    items = []
-
-    # Previous button
-    items =
-      if current_page > 1,
-        do: items ++ [%{type: "previous", number: current_page - 1}],
-        else: items
-
-    # Start pages
-    items =
-      Enum.reduce(start_pages, items, fn i, acc ->
-        acc ++ [%{type: "page", number: i, first: i == 1, only: total_pages == 1}]
-      end)
-
-    # First ellipsis
-    items =
-      if siblings_start > boundary_count + 2,
-        do: items ++ [%{type: "ellipsis"}],
-        else:
-          if(boundary_count + 1 < total_pages - boundary_count,
-            do: items ++ [%{type: "page", number: boundary_count + 1}],
-            else: items
-          )
-
-    # Siblings
-    items =
-      if siblings_start <= siblings_end do
-        Enum.reduce(siblings_start..siblings_end, items, fn i, acc ->
-          acc ++ [%{type: "page", number: i}]
-        end)
+    boundary_start =
+      if boundary_count > 0 do
+        1..min(boundary_count, start_siblings) |> Enum.to_list()
       else
-        items
+        [start_siblings]
       end
 
-    # Second ellipsis
-    items =
-      if siblings_end < total_pages - boundary_count - 1,
-        do: items ++ [%{type: "ellipsis"}],
-        else:
-          if(total_pages - boundary_count > boundary_count,
-            do: items ++ [%{type: "page", number: total_pages - boundary_count}],
-            else: items
-          )
+    siblings = start_siblings..end_siblings |> Enum.to_list()
 
-    # End pages
-    items =
-      if end_pages_start <= end_pages_end do
-        Enum.reduce(end_pages, items, fn i, acc ->
-          acc ++ [%{type: "page", number: i, last: i == total_pages}]
-        end)
+    boundary_end =
+      if boundary_count > 0 do
+        max(total_pages - boundary_count + 1, end_siblings)..total_pages |> Enum.to_list()
       else
-        items
+        [end_siblings]
       end
 
-    # Next button
-    items =
-      if current_page < total_pages,
-        do: items ++ [%{type: "next", number: current_page + 1}],
-        else: items
+    pages =
+      Enum.concat([boundary_start, siblings, boundary_end])
+      |> Enum.sort()
+      |> Enum.dedup()
+      |> Enum.to_list()
 
-    items
+    first_page = List.first(pages)
+    last_page = List.last(pages)
+
+    pages_next =
+      Enum.drop(pages, 1)
+      |> Enum.concat([List.last(pages) + 1])
+      |> Enum.to_list()
+
+    Enum.zip(pages, pages_next)
+    |> Enum.flat_map(fn t ->
+      case t do
+        {page, next} when next - page == 1 ->
+          [%{type: "page", number: page}]
+
+        {page, next} when next - page > 1 ->
+          [%{type: "page", number: page}, %{type: "ellipsis"}]
+
+        _ ->
+          []
+      end
+    end)
+    |> Enum.map(fn item ->
+      case item do
+        %{type: "page"} ->
+          item
+          |> Map.put(:first?, item.number == first_page)
+          |> Map.put(:current?, item.number == current_page)
+          |> Map.put(:last?, item.number == last_page)
+
+        _ ->
+          item
+      end
+    end)
+    |> Enum.flat_map(fn item ->
+      case item do
+        %{first?: true, current?: true, last?: true} when total_pages > 1 ->
+          [
+            get_prev_item(current_page, total_pages),
+            item,
+            get_next_item(current_page, total_pages)
+          ]
+
+        %{first?: true, last?: false} when total_pages > 1 ->
+          [
+            get_prev_item(current_page, total_pages),
+            item
+          ]
+
+        %{first?: false, last?: true} when total_pages > 1 ->
+          [
+            item,
+            get_next_item(current_page, total_pages)
+          ]
+
+        _ ->
+          [item]
+      end
+    end)
   end
 
-  defp get_box_class(item, is_active \\ false) do
+  defp get_prev_item(current_page, _total_pages) do
+    %{
+      type: "previous",
+      number: max(1, current_page - 1),
+      enabled?: current_page > 1
+    }
+  end
+
+  defp get_next_item(current_page, total_pages) do
+    %{
+      type: "next",
+      number: min(current_page + 1, total_pages),
+      enabled?: current_page < total_pages
+    }
+  end
+
+  defp get_box_class(item) do
     base_classes =
       "inline-flex items-center justify-center leading-5 px-3.5 py-2 border border-gray-200 dark:border-gray-700"
 
     active_classes =
-      if is_active,
+      if item.current?,
         do: "bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300",
         else:
           "bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-800 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-400"
 
     rounded_classes =
-      cond do
-        item[:only] ->
+      case item do
+        %{first?: true, last?: true} ->
           "rounded"
-
-        item[:first] ->
+        %{first?: true, last?: false} ->
           "rounded-l "
-
-        item[:last] ->
+        %{first?: false, last?: true} ->
           "rounded-r"
-
-        true ->
+        _ ->
           ""
       end
 
