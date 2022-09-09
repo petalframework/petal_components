44c05825eea24382dc982f9a22f26867136ba812
f0rest8 <markthayer@hey.com>
2022-09-08 22:10:57 -0400

    Rerun generator

--- lib/petal_components/icons/heroicons/outline.ex
+++ lib/petal_components/icons/heroicons/outline.ex
@@ -2,9 +2,9 @@ defmodule PetalComponents.Heroicons.Outline do
   @moduledoc """
   Icon name can be the function or passed in as a type eg.
 
-      <PetalComponents.Heroicons.Solid.home class="w-5 h-5" />
-      <PetalComponents.Heroicons.Solid.home title="Optional title for accessibility" class="w-5 h-5" />
-      <PetalComponents.Heroicons.Solid.render icon="home" class="w-5 h-5" />
+      <PetalComponents.Heroicons.Solid.home class="w-6 h-6" />
+      <PetalComponents.Heroicons.Solid.home title="Optional title for accessibility" class="w-6 h-6" />
+      <PetalComponents.Heroicons.Solid.render icon="home" class="w-6 h-6" />
 
       <PetalComponents.Heroicons.Mini.Solid.home class="w-5 h-5" />
       <PetalComponents.Heroicons.Mini.Solid.home title="Optional title for accessibility" class="w-5 h-5" />
