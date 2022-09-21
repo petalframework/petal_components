60fd6c5b9183f94cce55196c29cb0e2b54917042
Nicolas Hoban <nhobes@gmail.com>
2022-09-21 14:02:52 +1000

    Extendability updates

--- lib/petal_components/container.ex
+++ lib/petal_components/container.ex
@@ -16,7 +16,7 @@ defmodule PetalComponents.Container do
 
     ~H"""
     <div {@rest} class={build_class([
-      "mx-auto sm:px-6 lg:px-8 w-full",
+      "pc-container",
       get_width_class(@max_width),
       get_padding_class(@no_padding_on_mobile),
       @class
