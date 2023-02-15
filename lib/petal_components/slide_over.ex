b4bb0c8643f03bb0c932f8126828ee023a721592
Nicolas Hoban <nhobes@gmail.com>
2023-02-15 12:45:50 +1100

    slideover

--- lib/petal_components/slide_over.ex
+++ lib/petal_components/slide_over.ex
@@ -28,7 +28,7 @@ defmodule PetalComponents.SlideOver do
     <div {@rest} id="slide-over">
       <div
         id="slide-over-overlay"
-        class="fixed inset-0 z-50 transition-opacity bg-gray-900 dark:bg-gray-900 bg-opacity-30 dark:bg-opacity-70"
+        class="pc-slideover__overlay"
         aria-hidden="true"
       >
       </div>
@@ -36,7 +36,7 @@ defmodule PetalComponents.SlideOver do
       <div
         class={
           build_class([
-            "fixed inset-0 z-50 flex overflow-hidden transform",
+            "pc-slideover__wrapper",
             get_margin_classes(@origin),
             @class
           ])
@@ -52,22 +52,22 @@ defmodule PetalComponents.SlideOver do
           phx-key="escape"
         >
           <!-- Header -->
-          <div class="px-5 py-3 border-b border-gray-100 dark:border-gray-600">
-            <div class="flex items-center justify-between">
-              <div class="font-semibold text-gray-800 dark:text-gray-200">
+          <div class="pc-slideover__header">
+            <div class="pc-slideover__header__container">
+              <div class="pc-slideover__header__text">
                 <%= @title %>
               </div>
 
-              <button phx-click={hide_slide_over(@origin)} class="text-gray-400 hover:text-gray-500">
+              <button phx-click={hide_slide_over(@origin)} class="pc-slideover__header__button">
                 <div class="sr-only">Close</div>
-                <svg class="w-4 h-4 fill-current">
+                <svg class="pc-slideover__header__close-svg">
                   <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
                 </svg>
               </button>
             </div>
           </div>
           <!-- Content -->
-          <div class="p-5">
+          <div class="pc-slideover__content">
             <%= render_slot(@inner_block) %>
           </div>
         </div>
@@ -117,7 +117,7 @@ defmodule PetalComponents.SlideOver do
   end
 
   defp get_classes(max_width, origin, class) do
-    base_classes = "w-full max-h-full overflow-auto bg-white shadow-lg dark:bg-gray-800"
+    base_classes = "pc-slideover__box"
 
     slide_over_classes =
       case origin do
@@ -131,12 +131,12 @@ defmodule PetalComponents.SlideOver do
       case origin do
         x when x in ["left", "right"] ->
           case max_width do
-            "sm" -> "max-w-sm"
-            "md" -> "max-w-xl"
-            "lg" -> "max-w-3xl"
-            "xl" -> "max-w-5xl"
-            "2xl" -> "max-w-7xl"
-            "full" -> "max-w-full"
+            "sm" -> "pc-slideover__box--sm"
+            "md" -> "pc-slideover__box--md"
+            "lg" -> "pc-slideover__box--lg"
+            "xl" -> "pc-slideover__box--xl"
+            "2xl" -> "pc-slideover__box--2xl"
+            "full" -> "pc-slideover__box--full"
           end
 
         x when x in ["top", "bottom"] ->
