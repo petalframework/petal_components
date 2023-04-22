80b10f2619d17e68e32d7edf24c1bb9d934d6fec
Fred Wu <ifredwu@gmail.com>
2023-04-22 22:31:35 +1000

    Add an inner container to group the tooltip elements

--- lib/petal_components/button.ex
+++ lib/petal_components/button.ex
@@ -102,7 +102,6 @@ defmodule PetalComponents.Button do
       class={
         build_class([
           "pc-icon-button",
-          @tooltip && "relative group flex flex-col items-center",
           get_disabled_classes(@disabled),
           get_icon_button_background_color_classes(@color),
           get_icon_button_color_classes(@color),
@@ -113,18 +112,20 @@ defmodule PetalComponents.Button do
       disabled={@disabled}
       {@rest}
     >
-      <%= if @loading do %>
-        <Loading.spinner show={true} size_class={get_icon_button_spinner_size_classes(@size)} />
-      <% else %>
-        <%= render_slot(@inner_block) %>
+      <div class={@tooltip && "relative group flex flex-col items-center"}>
+        <%= if @loading do %>
+          <Loading.spinner show={true} size_class={get_icon_button_spinner_size_classes(@size)} />
+        <% else %>
+          <%= render_slot(@inner_block) %>
 
-        <div :if={@tooltip} role="tooltip" class="pc-icon-button__tooltip">
-          <span class="pc-icon-button__tooltip__text">
-            <%= @tooltip %>
-          </span>
-          <div class="pc-icon-button__tooltip__arrow"></div>
-        </div>
-      <% end %>
+          <div :if={@tooltip} role="tooltip" class="pc-icon-button__tooltip">
+            <span class="pc-icon-button__tooltip__text">
+              <%= @tooltip %>
+            </span>
+            <div class="pc-icon-button__tooltip__arrow"></div>
+          </div>
+        <% end %>
+      </div>
     </Link.a>
     """
   end
