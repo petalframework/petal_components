5679695e23dab68d1b3266575da04829df4e854b
Matthew Platts <plattsmw@gmail.com>
2023-05-26 11:42:25 +1000

    Fix formatter removing parens

--- .formatter.exs
+++ .formatter.exs
@@ -2,6 +2,7 @@
 [
   import_deps: [:phoenix],
   inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  locals_without_parens: [attr: 3, slot: 2, slot: 3],
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter]
 ]
