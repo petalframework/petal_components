0735caed18e5b3356a3085360d1c0833c3d28b57
Robin Boers <robindev2019@outlook.com>
2023-08-02 11:18:01 +0200

    Improve pagionation component

--- lib/petal_components/pagination.ex
+++ lib/petal_components/pagination.ex
@@ -6,23 +6,49 @@ defmodule PetalComponents.Pagination do
   import PetalComponents.Helpers
   import PetalComponents.PaginationInternal
 
-  attr(:path, :string, default: "/:page", doc: "page path")
-  attr(:class, :string, default: "", doc: "Parent div CSS class")
+  attr :path, :string, default: "/:page", doc: "page path"
+  attr :class, :string, default: "", doc: "Parent div CSS class"
 
-  attr(:link_type, :string,
+  attr :link_type, :string,
     default: "a",
     values: ["a", "live_patch", "live_redirect"]
-  )
 
-  attr(:total_pages, :integer, default: nil, doc: "sets a total page count")
-  attr(:current_page, :integer, default: nil, doc: "sets the current page")
-  attr(:sibling_count, :integer, default: 1, doc: "sets a sibling count")
-  attr(:boundary_count, :integer, default: 1, doc: "sets a boundary count")
-  attr(:rest, :global)
+  attr :total_pages, :integer, default: nil, doc: "sets a total page count"
+  attr :current_page, :integer, default: nil, doc: "sets the current page"
+  attr :sibling_count, :integer, default: 1, doc: "sets a sibling count"
+  attr :boundary_count, :integer, default: 1, doc: "sets a boundary count"
+  attr :rest, :global
+
+  slot :prev, required: false
+  slot :page, required: false
+  slot :next, required: false
 
   @doc """
   In the `path` param you can specify :page as the place your page number will appear.
   e.g "/posts/:page" => "/posts/1"
+
+  You can also use the `prev`, `page` and `next` slots to use your own buttons. This is completely
+  optional of course.
+
+  ## Example
+
+  <.pagination class="mb-5" current_page={@page} total_pages={@total_pages}>
+    <:prev>
+      <span phx-click="prev-page" class="pc-pagination__item__previous">
+        <Heroicons.chevron_left solid class="pc-pagination__item__previous__chevron" />
+      </span>
+    </:prev>
+    <:page :let={page}>
+      <span phx-click="to-page" phx-value-page={page}>
+        <%= page %>
+      </span>
+    </:page>
+    <:next>
+      <span phx-click="next-page" class="pc-pagination__item__next">
+        <Heroicons.chevron_right solid class="pc-pc-pagination__item__next__chevron" />
+      </span>
+    </:next>
+  </.pagination>
   """
 
   def pagination(assigns) do
@@ -32,13 +58,17 @@ defmodule PetalComponents.Pagination do
         <%= for item <- get_pagination_items(@total_pages, @current_page, @sibling_count, @boundary_count) do %>
           <%= if item.type == "prev" and item.enabled? do %>
             <div>
-              <Link.a
-                link_type={@link_type}
-                to={get_path(@path, item.number, @current_page)}
-                class="pc-pagination__item__previous"
-              >
-                <Heroicons.chevron_left solid class="pc-pagination__item__previous__chevron" />
-              </Link.a>
+              <%= if @prev do %>
+                <%= render_slot(@prev, item.number) %>
+              <% else %>
+                <Link.a
+                  link_type={@link_type}
+                  to={get_path(@path, item.number, @current_page)}
+                  class="pc-pagination__item__previous"
+                >
+                  <Heroicons.chevron_left solid class="pc-pagination__item__previous__chevron" />
+                </Link.a>
+              <% end %>
             </div>
           <% end %>
 
@@ -46,6 +76,9 @@ defmodule PetalComponents.Pagination do
             <li>
               <%= if item.current? do %>
                 <span class={get_box_class(item)}><%= item.number %></span>
+              <% else %>
+              <%= if @page do %>
+                <%= render_slot(@page, item.number) %>
               <% else %>
                 <Link.a
                   link_type={@link_type}
@@ -55,6 +88,7 @@ defmodule PetalComponents.Pagination do
                   <%= item.number %>
                 </Link.a>
               <% end %>
+            <% end %>
             </li>
           <% end %>
 
@@ -68,13 +102,17 @@ defmodule PetalComponents.Pagination do
 
           <%= if item.type == "next" and item.enabled? do %>
             <div>
-              <Link.a
-                link_type={@link_type}
-                to={get_path(@path, item.number, @current_page)}
-                class="pc-pagination__item__next"
-              >
-                <Heroicons.chevron_right solid class="pc-pagination__item__next__chevron" />
-              </Link.a>
+              <%= if @next do %>
+                <%= render_slot(@next, item.number) %>
+              <% else %>
+                <Link.a
+                  link_type={@link_type}
+                  to={get_path(@path, item.number, @current_page)}
+                  class="pc-pagination__item__next"
+                >
+                  <Heroicons.chevron_right solid class="pc-pagination__item__next__chevron" />
+                </Link.a>
+              <% end %>
             </div>
           <% end %>
         <% end %>
