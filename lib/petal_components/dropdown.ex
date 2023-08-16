ceb47ab0c4f767cfb78a5ed2560bcf6e760caf44
Robin Boers <robindev2019@outlook.com>
2023-08-16 13:25:42 +0200

    Add disabled option to dropdown item

--- lib/petal_components/dropdown.ex
+++ lib/petal_components/dropdown.ex
@@ -11,25 +11,25 @@ defmodule PetalComponents.Dropdown do
   @transition_out_start "transform opacity-100 scale-100"
   @transition_out_end "transform opacity-0 scale-95"
 
-  attr(:options_container_id, :string)
-  attr(:label, :string, default: nil, doc: "labels your dropdown option")
-  attr(:class, :string, default: "", doc: "any extra CSS class for the parent container")
+  attr :options_container_id, :string
+  attr :label, :string, default: nil, doc: "labels your dropdown option"
+  attr :class, :string, default: "", doc: "any extra CSS class for the parent container"
+  attr :disabled, :boolean, default: false
 
-  attr(:menu_items_wrapper_class, :string,
+  attr :menu_items_wrapper_class, :string,
     default: "",
     doc: "any extra CSS class for menu item wrapper container"
-  )
 
-  attr(:js_lib, :string,
+  attr :js_lib, :string,
     default: "alpine_js",
     values: ["alpine_js", "live_view_js"],
     doc: "javascript library used for toggling"
-  )
 
-  attr(:placement, :string, default: "left", values: ["left", "right"])
-  attr(:rest, :global)
-  slot(:trigger_element)
-  slot(:inner_block, required: false)
+  attr :placement, :string, default: "left", values: ["left", "right"]
+  attr :rest, :global
+
+  slot :trigger_element
+  slot :inner_block, required: false
 
   @doc """
     <.dropdown label="Dropdown" js_lib="alpine_js|live_view_js">
@@ -38,6 +38,7 @@ defmodule PetalComponents.Dropdown do
         Button item with icon
       </.dropdown_menu_item>
       <.dropdown_menu_item link_type="a" to="/" label="a item" />
+      <.dropdown_menu_item link_type="a" to="/" disabled label="disabled item" />
       <.dropdown_menu_item link_type="live_patch" to="/" label="Live Patch item" />
       <.dropdown_menu_item link_type="live_redirect" to="/" label="Live Redirect item" />
     </.dropdown>
@@ -56,9 +57,10 @@ defmodule PetalComponents.Dropdown do
       <div>
         <button
           type="button"
-          class={trigger_button_classes(@label, @trigger_element)}
+          class={[trigger_button_classes(@label, @trigger_element), @disabled && "pc-dropdown__menu-item--disabled"]}
           {js_attributes("button", @js_lib, @options_container_id)}
           aria-haspopup="true"
+          disabled={@disabled}
         >
           <span class="sr-only">Open options</span>
 
@@ -92,17 +94,16 @@ defmodule PetalComponents.Dropdown do
     """
   end
 
-  attr(:to, :string, default: nil, doc: "link path")
-  attr(:label, :string, doc: "link label")
-  attr(:class, :string, default: "", doc: "any additional CSS classes")
+  attr :to, :string, default: nil, doc: "link path"
+  attr :label, :string, doc: "link label"
+  attr :class, :string, default: "", doc: "any additional CSS classes"
 
-  attr(:link_type, :string,
+  attr :link_type, :string,
     default: "button",
     values: ["a", "live_patch", "live_redirect", "button"]
-  )
 
-  attr(:rest, :global, include: ~w(method download hreflang ping referrerpolicy rel target type))
-  slot(:inner_block, required: false)
+  attr :rest, :global, include: ~w(method download hreflang ping referrerpolicy rel target type)
+  slot :inner_block, required: false
 
   def dropdown_menu_item(assigns) do
     ~H"""
