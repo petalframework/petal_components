469166aa5bbeee4bd1eec08378e8ac10a8a77056
Nicolas Hoban <nhobes@gmail.com>
2023-09-13 14:03:18 +1000

    Changed close icon svg to Heroicons

--- lib/petal_components/modal.ex
+++ lib/petal_components/modal.ex
@@ -64,9 +64,7 @@ defmodule PetalComponents.Modal do
                 class="pc-modal__header__button"
               >
                 <div class="sr-only">Close</div>
-                <svg class="pc-modal__header__close-svg">
-                  <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
-                </svg>
+                <Heroicons.x_mark class="pc-modal__header__close-svg" />
               </button>
             </div>
           </div>
