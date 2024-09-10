9f5d361b34cd695429ff06bfaca73e7aa41518aa
Hamish Murphy <hamishmurphy@gmail.com>
2024-09-10 16:38:29 +1000

    mix format

--- test/petal/menu_test.exs
+++ test/petal/menu_test.exs
@@ -193,11 +193,18 @@ defmodule PetalComponents.MenuTest do
             path: "/sign-in",
             icon: fn assigns ->
               ~H"""
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
               </svg>
               """
-            end,
+            end
           }
         ],
         current_page: :current_page,
