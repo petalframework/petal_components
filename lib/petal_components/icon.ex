65c78b16dc866a618cfe745d384e0846293e054e
Hamish Murphy <hamishmurphy@gmail.com>
2024-09-19 16:49:20 +1000

    Improved error trapping for .icon

--- lib/petal_components/icon.ex
+++ lib/petal_components/icon.ex
@@ -3,17 +3,36 @@ defmodule PetalComponents.Icon do
 
   require Logger
 
-  deps_paths = Mix.Project.deps_paths()
-  heroicons_path = deps_paths[:heroicons]
+  deps_path = Mix.Project.deps_path()
+  heroicons_path = Path.join(deps_path, "heroicons/optimized")
 
   # Heroicons aren't available when pushing to hex
-  if !heroicons_path do
+  if !File.dir?(heroicons_path) do
+    Logger.warning("""
+    Heroicons path "#{heroicons_path}" does not exist or is not a folder. Consider adding the following to your list of dependencies:
+
+    {:heroicons,
+      github: "tailwindlabs/heroicons",
+      tag: "v2.1.5",
+      app: false,
+      compile: false,
+      sparse: "optimized"}
+
+    See https://petal.build/components/heroicons for up-to-date instructions
+    """)
+
     attr :rest, :global,
       doc: "the arbitrary HTML attributes for the heroicon container",
       include: ~w(role aria-hidden)
 
-    attr :name, :any, required: true
+    attr :name, :string, required: true
     attr :class, :any, default: nil, doc: "class applied to heroicon container"
+
+    def icon(%{name: name}) when is_atom(name) do
+      raise ArgumentError,
+        message:
+          ":#{name} is not a valid heroicon name. Please use a class name like \"hero-home\""
+    end
 
     def icon(%{name: "hero-" <> _} = assigns) do
       ~H"""
@@ -24,15 +43,15 @@ defmodule PetalComponents.Icon do
 
   # If heroicons are included as a dependency (either in :dev or in the
   # calling application), then generate a function per heroicon variant
-  if heroicons_path do
+  if File.dir?(heroicons_path) do
     attr :rest, :global,
       doc: "the arbitrary HTML attributes for the heroicon container",
       include: ~w(role aria-hidden)
 
-    attr :name, :any, required: true
+    attr :name, :string, required: true
     attr :class, :any, default: nil, doc: "class applied to heroicon container"
 
-    defp heroicon(%{name: "hero-" <> _} = assigns) do
+    defp heroicon(assigns) do
       ~H"""
       <span class={[@name, @class]} {@rest} />
       """
@@ -67,12 +86,24 @@ defmodule PetalComponents.Icon do
         doc: "the arbitrary HTML attributes for the svg container",
         include: ~w(role aria-hidden)
 
-      attr :name, :any, required: true
+      attr :name, :string, required: true
       attr :class, :any, default: nil, doc: "svg class"
+    end
+
+    def icon(%{name: name}) when is_atom(name) do
+      raise ArgumentError,
+        message:
+          ":#{name} is not a valid heroicon name. Please use a class name like \"hero-home\""
     end
 
     for heroicon_name <- heroicon_names do
       def icon(%{name: "hero-" <> unquote(heroicon_name)} = assigns), do: heroicon(assigns)
+    end
+
+    def icon(%{name: name}) do
+      raise ArgumentError,
+        message:
+          "\"#{name}\" is not a valid heroicon name. Please use a class name like \"hero-home\", \"hero-home-solid\", \"hero-home-mini\" or \"hero-home-micro\""
     end
   end
 end
