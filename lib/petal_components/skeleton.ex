1b8ce1cd125c919bc999487a694a62c86b3d4559
nhobes <nhobes@gmail.com>
2024-09-20 11:20:17 +1000

    Removes text placeholder from image skeleton to match video skeleton

--- lib/petal_components/skeleton.ex
+++ lib/petal_components/skeleton.ex
@@ -39,14 +39,6 @@ defmodule PetalComponents.Skeleton do
           <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
         </svg>
       </div>
-      <div class="w-full">
-        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
-        <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-      </div>
       <span class="sr-only">Loading...</span>
     </div>
     """
@@ -137,7 +129,7 @@ defmodule PetalComponents.Skeleton do
       <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
       <div class="flex items-center mt-4">
         <svg
-          class="w-10 h-10 me-3 text-gray-200 dark:text-gray-700"
+          class="w-10 h-10 text-gray-200 me-3 dark:text-gray-700"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -166,9 +158,9 @@ defmodule PetalComponents.Skeleton do
       <div class="w-48 h-2 mb-10 bg-gray-200 rounded-full dark:bg-gray-700"></div>
       <div class="flex items-baseline mt-4">
         <div class="w-full bg-gray-200 rounded-t-lg h-72 dark:bg-gray-700"></div>
-        <div class="w-full h-56 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"></div>
+        <div class="w-full h-56 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"></div>
         <div class="w-full bg-gray-200 rounded-t-lg h-72 ms-6 dark:bg-gray-700"></div>
-        <div class="w-full h-64 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"></div>
+        <div class="w-full h-64 bg-gray-200 rounded-t-lg ms-6 dark:bg-gray-700"></div>
         <div class="w-full bg-gray-200 rounded-t-lg h-80 ms-6 dark:bg-gray-700"></div>
         <div class="w-full bg-gray-200 rounded-t-lg h-72 ms-6 dark:bg-gray-700"></div>
         <div class="w-full bg-gray-200 rounded-t-lg h-80 ms-6 dark:bg-gray-700"></div>
