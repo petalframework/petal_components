6824cb540adf42e68f2a1150a48d13827ec3d746
Serge Aleynikov <saleyn@gmail.com>
2025-03-14 08:46:21 -0400

    Fix issues identified by the reviewer

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,4 @@ import Config
 
 config :phoenix, :json_library, Jason
 
-File.exists?("config/#{config_env()}.exs") && import_config "#{config_env()}.exs"
+if File.exists?("config/#{config_env()}.exs"), do: import_config "#{config_env()}.exs"
